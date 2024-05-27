f41683fef96b35577c32e77f054b4512330b9d81
Johan Garcia Trejo <johan.garcia.dev@gmail.com>
2024-05-26 22:13:35 -0600

    feat: :sparkles: Agregar maquetación básica HTML y lógica para la elección de magos
    - Creé los archivos HTML necesarios para la interfaz de usuario. - Implementé la funcionalidad para que los usuarios puedan elegir a su mago. - Agregué la lógica para que la PC también seleccione un mago al azar.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+const initGame = () => {
+  const selectWizard = document.getElementById("select-wizard");
 
+  selectWizard.addEventListener("click", selectWizards);
+};
+
+const selectWizards = () => {
+  selectWizardUser();
+  selectWizardEnemy();
+};
+
+const selectWizardUser = () => {
+  const wizardUser = document.querySelector(
+    "input[name='wizards']:checked"
+  ).value;
+  const allyWizard = document.getElementById("ally-wizard");
+
+  allyWizard.innerHTML = wizardUser;
+};
+
+const selectWizardEnemy = () => {
+  const enemyWizard = document.getElementById("enemy-wizard");
+  const wizards = ["Magd", "Flamewalker", "Stoneheart", "Whisperwind"];
+
+  enemyWizard.innerHTML = wizards[random(3, 0)];
+};
+
+const random = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+
+window.addEventListener("load", initGame);
