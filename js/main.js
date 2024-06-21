29f6a37d84e27bee4aada9f2e434532392c533f1
Johan Garcia Trejo <johan.garcia.dev@gmail.com>
2024-06-21 00:07:06 -0600

    feat: :sparkles: Implementar combate entre magos
    Se añade la funcionalidad de combate entre dos magos. Los jugadores ahora pueden lanzar hechizos. Se han implementado los siguientes cambios:  - Agregado sistema de ataques. - Creados ataques para los magos de agua, fuego, tierra y aire. - Mejorada la lógica de combate y cálculo de daño.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+import { attacksList, damageWizard, lives } from "./wizards.js";
+
 const initGame = () => {
   const selectWizard = document.getElementById("select-wizard");
 
   selectWizard.addEventListener("click", selectWizards);
-  attack();
+  selectAttackUser();
 };
 
 const selectWizards = () => {
@@ -14,10 +16,11 @@ const selectWizardUser = () => {
   const wizardUser = document.querySelector(
     "input[name='wizards']:checked"
   ).value;
-  const allyWizard = document.getElementById("ally-wizard");
+  const userWizard = document.getElementById("user-wizard");
 
-  selectAttack(wizardUser.toLowerCase());
-  allyWizard.innerHTML = wizardUser;
+  userWizard.innerHTML = wizardUser;
+  showAttacks(wizardUser.toLowerCase());
+  showLife(wizardUser, 1);
 };
 
 const selectWizardEnemy = () => {
@@ -26,19 +29,20 @@ const selectWizardEnemy = () => {
   const wizardSelected = wizards[random(3, 0)];
 
   enemyWizard.innerHTML = wizardSelected;
+  showLife(wizardSelected, 2);
 };
 
-const selectAttack = (wizard) => {
+const showAttacks = (wizard) => {
   const attack1 = document.getElementById("btn-attack1");
   const attack2 = document.getElementById("btn-attack2");
-  const magd = ["Hidrobol", "Crioesfera"];
-  const flamewalker = ["Llamarada", "Ignición"];
-  const stoneheart = ["Rocío sísmico", "Túmulo"];
-  const whisperwind = ["Zumbido", "Tornado"];
-  const attacks = { magd, flamewalker, stoneheart, whisperwind };
+
+  let attacks = attacksList();
 
   let attackValue1 = attacks[wizard][0];
   let attackValue2 = attacks[wizard][1];
+
+  attack1.style.display = "inline-block";
+  attack2.style.display = "inline-block";
 
   attack1.innerHTML = attackValue1;
   attack2.innerHTML = attackValue2;
@@ -47,17 +51,101 @@ const selectAttack = (wizard) => {
   attack2.setAttribute("value", attackValue2);
 };
 
-const attack = () => {
+const selectAttackUser = () => {
+  const userWizard = document.getElementById("user-wizard");
   const attack1 = document.getElementById("btn-attack1");
   const attack2 = document.getElementById("btn-attack2");
 
-  attack1.addEventListener("click", () =>
-    console.log(`Ataca con ${attack1.getAttribute("value")}`)
+  attack1.addEventListener("click", () => {
+    const [enemyWizard, enemyAttack] = selectAttackEnemy();
+    const userWizard1 = userWizard.textContent;
+    const userAttack = attack1.getAttribute("value");
+
+    createMessages(userWizard1, userAttack, enemyWizard, enemyAttack);
+    validateWinner(enemyWizard, userWizard1, userAttack, enemyAttack);
+  });
+
+  attack2.addEventListener("click", () => {
+    const [enemyWizard, enemyAttack] = selectAttackEnemy();
+    const userWizard2 = userWizard.textContent;
+    const userAttack = attack2.getAttribute("value");
+
+    createMessages(userWizard2, userAttack, enemyWizard, enemyAttack);
+    validateWinner(enemyWizard, userWizard2, userAttack, enemyAttack);
+  });
+};
+
+const selectAttackEnemy = () => {
+  const enemyWizard = document.getElementById("enemy-wizard").textContent;
+  const enemy = enemyWizard.toLowerCase();
+  const attacks = attacksList();
+
+  const attack = attacks[enemy][random(1, 0)];
+
+  return [enemyWizard, attack];
+};
+
+const createMessages = (userWizard, userAttack, enemyWizard, enemyAttack) => {
+  const messages = document.getElementById("messages");
+  const message = document.createElement("p");
+  message.textContent = `El mago ${userWizard} ataco con ${userAttack} y el mago ${enemyWizard} ataco con ${enemyAttack}`;
+
+  messages.appendChild(message);
+};
+
+const showLife = (wizard, id) => {
+  const lifeWizard = lives(wizard);
+  const livesContainer = document.getElementById("lives");
+  const wizardLife = document.createElement("p");
+
+  wizardLife.id = `${wizard}-${id}`;
+  wizardLife.textContent = lifeWizard;
+
+  livesContainer.appendChild(wizardLife);
+};
+
+const updateLife = (id, damaged, attacker, attack) => {
+  const lifeWizard = document.getElementById(`${damaged}-${id}`);
+  let lifeWizardValue = lifeWizard.textContent;
+
+  const damage = damageWizard(damaged, attacker, attack);
+  lifeWizardValue -= damage;
+
+  console.log(`Damaged: ${damaged}, attacker: ${attacker}, attack: ${attack}`);
+
+  return [lifeWizard, lifeWizardValue];
+};
+
+const validateWinner = (damaged, attacker, attackUser, attackEnemy) => {
+  const [lifeWizardEnemy, lifeWizardEnemyValue] = updateLife(
+    2,
+    damaged,
+    attacker,
+    attackUser
   );
 
-  attack2.addEventListener("click", () =>
-    console.log(`Ataca con ${attack2.getAttribute("value")}`)
+  const [lifeWizardUser, lifeWizardUserValue] = updateLife(
+    1,
+    attacker,
+    damaged,
+    attackEnemy
   );
+
+  console.log(lifeWizardUserValue);
+  console.log(lifeWizardEnemyValue);
+
+  if (lifeWizardUserValue > 0 && lifeWizardEnemyValue > 0) {
+    lifeWizardUser.textContent = lifeWizardUserValue;
+    lifeWizardEnemy.textContent = lifeWizardEnemyValue;
+  } else {
+    if (lifeWizardUserValue < 0) {
+      lifeWizardUser.textContent = 0;
+      alert(`El mago ${damaged} gana el Jugador Pierde`);
+    } else if (lifeWizardEnemyValue < 0) {
+      lifeWizardEnemy.textContent = 0;
+      alert(`El mago ${attacker} gana la PC pierde`);
+    }
+  }
 };
 
 const random = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
---
+++ js/wizards.js
@@ -0,0 +1,101 @@
+const attacksList = () => {
+  const magd = ["Burbuja", "Glacial"];
+  const flamewalker = ["Llamarada", "Inferno"];
+  const stoneheart = ["Pedrisco", "Monolito"];
+  const whisperwind = ["Zumbido", "Tornado"];
+  const attacks = { magd, flamewalker, stoneheart, whisperwind };
+
+  return attacks;
+};
+
+const damageWizard = (damaged, attacker, attack) => {
+  const damage = {
+    Magd: {
+      Magd: {
+        Burbuja: 100,
+        Glacial: 175,
+      },
+      Flamewalker: {
+        Llamarada: 375,
+        Inferno: 500,
+      },
+      Stoneheart: {
+        Pedrisco: 250,
+        Monolito: 325,
+      },
+      Whisperwind: {
+        Zumbido: 200,
+        Tornado: 275,
+      },
+    },
+    Flamewalker: {
+      Magd: {
+        Burbuja: 400,
+        Glacial: 550,
+      },
+      Flamewalker: {
+        Llamarada: 100,
+        Inferno: 175,
+      },
+      Stoneheart: {
+        Pedrisco: 125,
+        Monolito: 200,
+      },
+      Whisperwind: {
+        Zumbido: 175,
+        Tornado: 225,
+      },
+    },
+    Stoneheart: {
+      Magd: {
+        Burbuja: 140,
+        Glacial: 200,
+      },
+      Flamewalker: {
+        Llamarada: 175,
+        Inferno: 200,
+      },
+      Stoneheart: {
+        Pedrisco: 100,
+        Monolito: 175,
+      },
+      Whisperwind: {
+        Zumbido: 300,
+        Tornado: 475,
+      },
+    },
+    Whisperwind: {
+      Magd: {
+        Burbuja: 150,
+        Glacial: 225,
+      },
+      Flamewalker: {
+        Llamarada: 175,
+        Inferno: 240,
+      },
+      Stoneheart: {
+        Pedrisco: 350,
+        Monolito: 425,
+      },
+      Whisperwind: {
+        Zumbido: 100,
+        Tornado: 175,
+      },
+    },
+  };
+
+  return damage[damaged][attacker][attack];
+};
+
+const lives = (wizard) => {
+  const lifeWizards = {
+    Magd: 1020,
+    Flamewalker: 1033,
+    Stoneheart: 1010,
+    Whisperwind: 1000,
+  };
+
+  return lifeWizards[wizard];
+};
+
+export { attacksList, damageWizard, lives };
